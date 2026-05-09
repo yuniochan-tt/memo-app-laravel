fd79a5e49837312bdd740e8b3f1a25972a482fce
yuniochan-tt <ib250015yen@setouchicollege.com>
2026-05-09 14:53:43 +0900

    step9

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Auth;
 */
 Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
 Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
+Route::post('/user/register', 'App\Http\Controllers\Auth\RegisterController@register')
+    ->name('user.exec.register');
 Route::get('/memo', function() {
     return view("memo");
 })->name('memo.index');
