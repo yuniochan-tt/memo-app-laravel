245629b4c18687f95b1089180019273de2e93548
yuniochan-tt <ib250015yen@setouchicollege.com>
2026-05-30 15:06:19 +0900

    update

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request; 
 
 class LoginController extends Controller
 {
@@ -22,14 +23,34 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * 認証成功後のリダイレクト先遷移パス
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/memo'; 
 
     /**
-     * Create a new controller instance.
+     * ログイン時のバリデーション（トレイトのメソッドをオーバーライド）
+     * * @param Request $request
+     */
+    protected function validateLogin(Request $request)
+    {
+        // ログインバリデーション実行前に言語設定を日本語に強制する
+        app()->setLocale('ja');
+
+        $request->validate(
+            [
+                $this->username() => 'required|max:255|email',
+                'password'        => 'required|min:8|max:255|regex:/^[a-zA-Z0-9]+$/',
+            ],
+            [
+                'password.regex' => ':attributeは半角英数字で入力してください。'
+            ]
+        );
+    }
+
+    /**
+     * 新しいコントローラーインスタンスの生成
      *
      * @return void
      */
